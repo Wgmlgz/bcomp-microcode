9a99a448efe80e0a6592a0df5b159d96125611a3
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2023-05-17 00:24:34 +0300

    Updates

--- _app/immutable/chunks/stores.eeb9f1a2.js
+++
@@ -1,1 +0,0 @@
-import"./index.e1b34d23.js";import{s as e}from"./singletons.93fdd3e5.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
